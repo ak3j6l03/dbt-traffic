c5d368b519aa99ac6a97447e94393e1e1cb6247d
Jade Lee <jlee@cims-global.com>
2026-03-09 16:17:25 +0000

    Initial commit

---
+++ models/staging/stg_traffic_offenders.sql
@@ -0,0 +1,19 @@
+with source as (
+    select *
+    from {{ source('processed', 'traffic_processed_2025') }}
+),
+
+cleaned as (
+    select
+        cast(violation_year_month as string) as violation_year_month,
+        cast(age as int) as age,
+        cast(gender as string) as gender,
+        cast(violation_type as string) as violation_type,
+        cast(roc_year as int) as roc_year,
+        cast(month as int) as month,
+        cast(year as int) as year
+    from source
+)
+
+select *
+from cleaned
